3ad251986309acbc07924e4f9cb94b6a02540cba
Ruben Pla Ferrero <ruben.f@connectik.com>
2015-11-19 18:57:22 +0100

    new constants utils for save instance states

--- app/src/main/java/org/npawstreamer/npaw_streamer/utils/Const.java
+++ app/src/main/java/org/npawstreamer/npaw_streamer/utils/Const.java
@@ -2,9 +2,13 @@ package org.npawstreamer.npaw_streamer.utils;
 
 public final class Const
 {
+    //URLS
     public static final String URL_MOVIE_ONE = "http://cdn.s1.eu.nice264.com/converted_work6/0082c06e504b0a422bf1_6815f2deeb179c29748af42f8cd5ce95.mp4";
-
     public static final String URL_MOVIE_TWO = "http://cdn.s2.eu.nice264.com/converted_work6/0082c06e504b0a422bf1_6815f2deeb179c29748af42f8cd5ce95.mp4";
 
+    //PARAMS CONSTANTS
     public static final String EXTRA_MOVIE_PARAM = "EXTRA_MOVIE_PARAM";
+    public static final String EXTRA_MOVIE_URL = "EXTRA_MOVIE_PARAM";
+    public static final String EXTRA_MOVIE_ISPLAYING = "EXTRA_MOVIE_ISPLAYING";
+    public static final String EXTRA_MOVIE_POSITION = "EXTRA_MOVIE_POSITION";
 }
