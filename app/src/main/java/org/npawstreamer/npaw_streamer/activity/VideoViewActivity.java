fed90a610cccf777aa2974e03203e58ef7173d32
Ruben Pla Ferrero <ruben.f@connectik.com>
2015-11-23 09:16:56 +0100

    VideoView improvements, not finished

--- app/src/main/java/org/npawstreamer/npaw_streamer/activity/VideoViewActivity.java
+++ app/src/main/java/org/npawstreamer/npaw_streamer/activity/VideoViewActivity.java
@@ -1,19 +1,42 @@
 package org.npawstreamer.npaw_streamer.activity;
 
 import android.content.res.Configuration;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import org.npawstreamer.npaw_streamer.R;
+import org.npawstreamer.npaw_streamer.utils.Const;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import callbacks.NpawMediaPlayerCallbacks;
+import view.NpawMediaPlayer;
 import view.NpawVideoView;
 
-public class VideoViewActivity extends AppCompatActivity
+public class VideoViewActivity extends AppCompatActivity implements NpawMediaPlayer.OnErrorListener
 {
+    public final static String TAG = VideoViewActivity.class.getSimpleName();
+
+    String videoUri;
+
     @Bind(R.id.view_video)
-    NpawVideoView videoView;
+    NpawVideoView mVideoView;
+
+    @Bind(R.id.vv_progressbar)
+    ProgressBar progressBar;
+
+//    private ProgressBar progressBar;
+
+    private MediaController.MediaPlayerControl mMediaPlayerControl;
+    private MediaController mMediaController;
+    private NpawMediaPlayerCallbacks videoViewCallBackListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -21,12 +44,91 @@ public class VideoViewActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_view);
 
+        final String videoUri = getIntent().getStringExtra(Const.EXTRA_MOVIE_PARAM);
+
         ButterKnife.bind(this);
+
+        mMediaPlayerControl = mVideoView;
+        mMediaController = new MediaController(this);
+        mMediaController.setAnchorView(findViewById(R.id.videoview_container));
+        mMediaController.setMediaPlayer(mMediaPlayerControl);
+        mMediaController.setEnabled(false);
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        if (savedInstanceState != null)
+        {
+            init(savedInstanceState.getString(Const.EXTRA_MOVIE_URL),
+                    savedInstanceState.getInt(Const.EXTRA_MOVIE_POSITION),
+                    savedInstanceState.getBoolean(Const.EXTRA_MOVIE_ISPLAYING));
+        } else
+        {
+            init(videoUri, -1, false);
+        }
+    }
+
+    private void init(String uri, final int position, final boolean playback)
+    {
+        videoUri = uri;
+
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener()
+        {
+            @Override
+            public void onPrepared(MediaPlayer mp)
+            {
+                mVideoView.start();
+
+                progressBar.setVisibility(View.GONE);
+            }
+
+        });
+
+        mVideoView.getMediaPlayer().setErrorListener(new MediaPlayer.OnErrorListener() {
+            @Override
+            public boolean onError(MediaPlayer mp, int what, int extra) {
+                Toast.makeText(VideoViewActivity.this,
+                        "Cannot play the video, see logcat for the detailed exception",
+                        Toast.LENGTH_LONG).show();
+                progressBar.setVisibility(View.GONE);
+                mMediaController.setEnabled(false);
+                return true;
+            }
+        });
+        mVideoView.getMediaPlayer().setInfoListener(new MediaPlayer.OnInfoListener()
+        {
+            @Override
+            public boolean onInfo(MediaPlayer mp, int what, int extra)
+            {
+                mVideoView.getMediaPlayer().filterOnInfoListenerResult(what, extra);
+                return true;
+            }
+        });
+
+        mVideoView.setOnSeekCompleteListener(new MediaPlayer.OnSeekCompleteListener()
+        {
+            @Override
+            public void onSeekComplete(MediaPlayer mp)
+            {
+                Log.d(TAG, "onSeekComplete");
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+        mVideoView.setOnBufferingUpdateListener(new MediaPlayer.OnBufferingUpdateListener()
+        {
+            @Override
+            public void onBufferingUpdate(MediaPlayer mp, int percent)
+            {
+                Log.d(TAG, "onBufferingUpdate " + percent + "%");
+            }
+        });
+
+        mVideoView.setVideoSource(videoUri);
     }
 
     @Override
     protected void onStop()
     {
+        mMediaController.hide();
         super.onStop();
     }
 
@@ -52,6 +154,12 @@ public class VideoViewActivity extends AppCompatActivity
     protected void onSaveInstanceState(Bundle outState)
     {
         super.onSaveInstanceState(outState);
+        if (videoUri != null)
+        {
+//            outState.putString(Const.EXTRA_MOVIE_URL, videoUri);
+//            outState.putBoolean(Const.EXTRA_MOVIE_ISPLAYING, mVideoView.isPlaying());
+//            outState.putInt(Const.EXTRA_MOVIE_POSITION, mVideoView.getCurrentPosition());
+        }
     }
 
     @Override
@@ -64,6 +172,21 @@ public class VideoViewActivity extends AppCompatActivity
     protected void onPause()
     {
         super.onPause();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        Log.d(TAG, "surfaceHolder.callBack.onTouchEvent()");
+       mMediaController.show();
+        return false;
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mp, int what, int extra)
+    {
+        videoViewCallBackListener.mpErrorFilterResult(what, extra);
+        return true;
     }
 
 
--- npawstreamerplugin/src/main/java/view/NpawVideoView.java
+++ npawstreamerplugin/src/main/java/view/NpawVideoView.java
@@ -2,40 +2,104 @@ package view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.MediaController;
 
 
-public class NpawVideoView extends SurfaceView implements SurfaceHolder.Callback,
-        MediaController.MediaPlayerControl
+public class NpawVideoView extends SurfaceView implements SurfaceHolder.Callback, android.widget.MediaController.MediaPlayerControl
 {
     private static final String TAG = NpawVideoView.class.getSimpleName();
+    private Context mContext;
+
+    private NpawMediaPlayer.OnPreparedListener mOnPreparedListener;
+    private NpawMediaPlayer.OnSeekListener mOnSeekListener;
+    private NpawMediaPlayer.OnSeekCompleteListener mOnSeekCompleteListener;
+    private NpawMediaPlayer.OnCompletionListener mOnCompletionListener;
+    private NpawMediaPlayer.OnErrorListener mOnErrorListener;
+    private NpawMediaPlayer.OnInfoListener mOnInfoListener;
+    private NpawMediaPlayer.OnBufferingUpdateListener mOnBufferingUpdateListener;
 
     private NpawMediaPlayer mPlayer;
+    private MediaController mMediaController;
     private SurfaceHolder mSurfaceHolder;
+    private String videoUrl;
     private int mVideoWidth;
     private int mVideoHeight;
+
+    private NpawMediaPlayer.OnVideoSizeChangedListener mOnVideoSizeChangedListener;
 
     public NpawVideoView(Context context)
     {
         super(context);
+        mContext = context;
+        init();
     }
 
     public NpawVideoView(Context context, AttributeSet attrs)
     {
         super(context, attrs);
+        mContext = context;
+        init();
     }
 
     public NpawVideoView(Context context, AttributeSet attrs, int defStyleAttr)
     {
         super(context, attrs, defStyleAttr);
+        mContext = context;
+        init();
+    }
+
+    private void init()
+    {
+        Log.i(TAG, "init()");
+
+        getHolder().addCallback(this);
+    }
+
+    public void setVideoSource(String url)
+    {
+        Log.i(TAG, "setVideoSource()");
+        videoUrl = url;
+//        mSeekWhenPrepared = 0;
+        launchVideo();
+        requestLayout();
+        invalidate();
+    }
+
+    private void launchVideo()
+    {
+        if (mPlayer != null)
+        {
+            mPlayer.release();
+            mPlayer = null;
+        }
+
+        mPlayer = new NpawMediaPlayer();
+        mPlayer.initMediaPlayer(mContext, videoUrl);
+        mPlayer.setDisplay(mSurfaceHolder);
+        mPlayer.setMediaPlayerAdjustements();
+//        mPlayer.setOnVideoSizeChangedListener(mOnVideoSizeChangedListener);
+//        mPlayer.setOnPreparedListener(mOnPreparedListener);
+//        mPlayer.setOnSeekListener(mOnSeekListener);
+//        mPlayer.setOnSeekCompleteListener(mOnSeekCompleteListener);
+//        mPlayer.setOnCompletionListener(mOnCompletionListener);
+//        mPlayer.setOnErrorListener(mOnErrorListener);
+//        mPlayer.setOnInfoListener(mOnInfoListener);
+//        mPlayer.setOnBufferingUpdateListener(mOnBufferingUpdateListener);
+    }
+
+    public NpawMediaPlayer getMediaPlayer()
+    {
+        return mPlayer;
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder)
     {
-
+        mSurfaceHolder = holder;
+        launchVideo();
     }
 
     @Override
@@ -47,43 +111,48 @@ public class NpawVideoView extends SurfaceView implements SurfaceHolder.Callback
     @Override
     public void surfaceDestroyed(SurfaceHolder holder)
     {
-
+        mSurfaceHolder = null;
+        if (mPlayer != null)
+        {
+            mPlayer.release();
+            mPlayer = null;
+        }
     }
 
     @Override
     public void start()
     {
-
+        mPlayer.start();
     }
 
     @Override
     public void pause()
     {
-
+        mPlayer.pause();
     }
 
     @Override
     public int getDuration()
     {
-        return 0;
+        return mPlayer.getDuration();
     }
 
     @Override
     public int getCurrentPosition()
     {
-        return 0;
+        return mPlayer.getCurrentPosition();
     }
 
     @Override
     public void seekTo(int pos)
     {
-
+        mPlayer.seekTo(pos);
     }
 
     @Override
     public boolean isPlaying()
     {
-        return false;
+        return true;
     }
 
     @Override
@@ -95,24 +164,60 @@ public class NpawVideoView extends SurfaceView implements SurfaceHolder.Callback
     @Override
     public boolean canPause()
     {
-        return false;
+        return true;
     }
 
     @Override
     public boolean canSeekBackward()
     {
-        return false;
+        return true;
     }
 
     @Override
     public boolean canSeekForward()
     {
-        return false;
+        return true;
     }
 
     @Override
     public int getAudioSessionId()
     {
-        return 0;
+        return mPlayer.getAudioSessionId();
     }
+
+    public void setOnPreparedListener(NpawMediaPlayer.OnPreparedListener l)
+    {
+        this.mOnPreparedListener = l;
+    }
+
+//    public void setOnSeekListener(MediaPlayer.OnSeekListener l) {
+//        this.mOnSeekListener = l;
+//    }
+
+    public void setOnSeekCompleteListener(NpawMediaPlayer.OnSeekCompleteListener l)
+    {
+        this.mOnSeekCompleteListener = l;
+    }
+
+    public void setOnCompletionListener(NpawMediaPlayer.OnCompletionListener l)
+    {
+        this.mOnCompletionListener = l;
+    }
+
+    public void setOnBufferingUpdateListener(NpawMediaPlayer.OnBufferingUpdateListener l)
+    {
+        this.mOnBufferingUpdateListener = l;
+    }
+
+    public void setOnErrorListener(NpawMediaPlayer.OnErrorListener l)
+    {
+        this.mOnErrorListener = l;
+    }
+
+    public void setOnInfoListener(NpawMediaPlayer.OnInfoListener l)
+    {
+        this.mOnInfoListener = l;
+    }
+
+
 }
